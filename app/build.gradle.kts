11520a0bffb2af8986749256a18247093abb6d72
amine <aytacamine633@gmail.com>
2024-01-31 22:44:23 +0300

    Update sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.amineaytac.biblictora"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.amineaytac.biblictora"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
