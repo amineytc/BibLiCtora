94c714087387d2df3db229fb3bc1890cf68cd506
amine <aytacamine633@gmail.com>
2024-02-01 17:59:27 +0300

    Create Home, Favorite and MyBooks Fragments

---
+++ app/src/main/java/com/amineaytac/biblictora/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,7 @@
+package com.amineaytac.biblictora.ui.favorite
+
+import androidx.fragment.app.Fragment
+import com.amineaytac.biblictora.R
+
+class FavoriteFragment : Fragment(R.layout.fragment_favorite)
+
---
+++ app/src/main/java/com/amineaytac/biblictora/ui/home/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.amineaytac.biblictora.ui.home
+
+import androidx.fragment.app.Fragment
+import com.amineaytac.biblictora.R
+
+class HomeFragment : Fragment(R.layout.fragment_home)
