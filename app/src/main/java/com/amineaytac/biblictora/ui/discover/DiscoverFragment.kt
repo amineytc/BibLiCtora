d46a539774db4bc0684de38d68945a520e42224f
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-02-05 00:22:11 +0300

    Create Discover and Reading List Fragments

---
+++ app/src/main/java/com/amineaytac/biblictora/ui/discover/DiscoverFragment.kt
@@ -0,0 +1,6 @@
+package com.amineaytac.biblictora.ui.discover
+
+import androidx.fragment.app.Fragment
+import com.amineaytac.biblictora.R
+
+class DiscoverFragment : Fragment(R.layout.fragment_discover)
