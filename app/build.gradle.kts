83b302d92e7180516c1e300b40672b7c9bcc8131
amine <aytacamine633@gmail.com>
2024-02-06 16:05:05 +0300

    Add Retrofit Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,10 @@ dependencies {
     // Hilt
     implementation("com.google.dagger:hilt-android:2.47")
     ksp("com.google.dagger:hilt-compiler:2.47")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
