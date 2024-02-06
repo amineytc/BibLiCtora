25ec7b73966cf84399e2a1888fc8b040db20b756
amine <aytacamine633@gmail.com>
2024-02-06 15:27:47 +0300

    Create Book Response Data Class

---
+++ app/src/main/java/com/amineaytac/biblictora/core/network/dto/Formats.kt
@@ -0,0 +1,29 @@
+package com.amineaytac.biblictora.core.network.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Formats(
+    @SerializedName("application/epub+zip")
+    val applicationepubzip: String?,
+    @SerializedName("application/octet-stream")
+    val applicationoctetStream: String?,
+    @SerializedName("application/rdf+xml")
+    val applicationrdfxml: String?,
+    @SerializedName("application/x-mobipocket-ebook")
+    val applicationxMobipocketEbook: String?,
+    @SerializedName("image/jpeg")
+    val imagejpeg: String?,
+    @SerializedName("text/html")
+    val texthtml: String?,
+    @SerializedName("text/html; charset=iso-8859-1")
+    val texthtmlCharsetiso88591: String?,
+    @SerializedName("text/html; charset=utf-8")
+    val texthtmlCharsetutf8: String?,
+    @SerializedName("text/plain; charset=iso-8859-1")
+    val textplainCharsetiso88591: String?,
+    @SerializedName("text/plain; charset=us-ascii")
+    val textplainCharsetusAscii: String?,
+    @SerializedName("text/plain; charset=utf-8")
+    val textplainCharsetutf8: String?
+)
---
+++ app/src/main/java/com/amineaytac/biblictora/core/network/dto/Translator.kt
@@ -0,0 +1,13 @@
+package com.amineaytac.biblictora.core.network.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Translator(
+    @SerializedName("birth_year")
+    val birthYear: Int?,
+    @SerializedName("death_year")
+    val deathYear: Int?,
+    @SerializedName("name")
+    val name: String?
+)
