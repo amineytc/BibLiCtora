2c2da71879210b907000b699670feaeb2c0b270e
amine <aytacamine633@gmail.com>
2024-09-17 14:08:57 +0300

    Complete DiscoverFragment BackDrop Back Layer UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
 
     //Material
     implementation("com.google.android.material:material:1.12.0-alpha03")
+    implementation("com.google.android.material:material:1.4.0")
 
     //SSP & SDP
     implementation("com.intuit.ssp:ssp-android:1.1.0")
@@ -71,4 +72,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    //Layout
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 }
