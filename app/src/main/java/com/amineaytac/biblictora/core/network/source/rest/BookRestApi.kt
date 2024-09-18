46760b891948f8f5d662004fdd71834c36aed20f
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-09-18 14:35:58 +0300

    Add getBooksWithSearch and getBooksWithLanguages suspend function in BookRestApi

--- app/src/main/java/com/amineaytac/biblictora/core/network/source/rest/BookRestApi.kt
+++ app/src/main/java/com/amineaytac/biblictora/core/network/source/rest/BookRestApi.kt
@@ -3,8 +3,15 @@ package com.amineaytac.biblictora.core.network.source.rest
 import com.amineaytac.biblictora.core.network.dto.BookResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface BookRestApi {
     @GET(".")
     suspend fun getAllBooks(): Response<BookResponse>
+
+    @GET(".")
+    suspend fun getBooksWithSearch(@Query("search") search: String): Response<BookResponse>
+
+    @GET(".")
+    suspend fun getBooksWithLanguages(@Query("languages") languages: List<String>): Response<BookResponse>
 }
