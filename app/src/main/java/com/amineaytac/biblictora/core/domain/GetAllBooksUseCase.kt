95403d61bfe2b8ad3200d485ca6c2bf423261cd7
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-09-24 13:19:53 +0300

    Edit functions in UseCases for adapt paging

--- app/src/main/java/com/amineaytac/biblictora/core/domain/GetAllBooksUseCase.kt
+++ app/src/main/java/com/amineaytac/biblictora/core/domain/GetAllBooksUseCase.kt
@@ -1,13 +1,13 @@
 package com.amineaytac.biblictora.core.domain
 
-import com.amineaytac.biblictora.core.common.ResponseState
+import androidx.paging.PagingData
 import com.amineaytac.biblictora.core.data.model.Book
 import com.amineaytac.biblictora.core.data.repo.BookRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetAllBooksUseCase @Inject constructor(private val bookRepository: BookRepository) {
-    suspend operator fun invoke(): Flow<ResponseState<List<Book>>> {
-        return bookRepository.getAllBooks()
+    suspend operator fun invoke(): Flow<PagingData<Book>> {
+        return bookRepository.getAllBooks("getAllBooks")
     }
 }
