af3d8a3fff388c5e1360e2026c95220349012f49
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-09-26 01:45:04 +0300

    Create BookDetailFragment

---
+++ app/src/main/java/com/amineaytac/biblictora/ui/detail/BookDetailFragment.kt
@@ -0,0 +1,6 @@
+package com.amineaytac.biblictora.ui.detail
+
+import androidx.fragment.app.Fragment
+import com.amineaytac.biblictora.R
+
+class BookDetailFragment : Fragment(R.layout.fragment_book_detail)
