29c2d2938eefc6641628b72e69fb3f9295367348
amine <aytacamine633@gmail.com>
2024-10-02 11:37:45 +0300

    Create FavoriteDao

---
+++ app/src/main/java/com/amineaytac/biblictora/core/database/di/FavoriteDao.kt
@@ -0,0 +1,21 @@
+package com.amineaytac.biblictora.core.database.di
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addFavoriteItem(favoriteEntity: FavoriteEntity)
+
+    @Delete
+    suspend fun deleteFavoriteItem(favoriteEntity: FavoriteEntity)
+
+    @Query("SELECT * FROM favorite_table")
+    fun getFavoriteItems() : Flow<List<FavoriteEntity>>
+}
