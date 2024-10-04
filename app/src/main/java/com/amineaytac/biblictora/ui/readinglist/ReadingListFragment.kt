ac2505c169022881bf19391ff673a64814b15f16
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-10-04 21:54:29 +0300

    Observe View Model data in ReadingListFragment

--- app/src/main/java/com/amineaytac/biblictora/ui/readinglist/ReadingListFragment.kt
+++ app/src/main/java/com/amineaytac/biblictora/ui/readinglist/ReadingListFragment.kt
@@ -1,6 +1,38 @@
 package com.amineaytac.biblictora.ui.readinglist
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.amineaytac.biblictora.R
+import com.amineaytac.biblictora.databinding.FragmentReadingListBinding
+import com.amineaytc.biblictora.util.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class ReadingListFragment : Fragment(R.layout.fragment_reading_list)
+@AndroidEntryPoint
+class ReadingListFragment : Fragment(R.layout.fragment_reading_list) {
+
+    private val binding by viewBinding(FragmentReadingListBinding::bind)
+    private val viewModel: ReadingListViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getReadingBooks()
+        observeUi()
+    }
+
+    private fun observeUi() = with(binding) {
+        viewModel.readingBookScreenUiState.observe(viewLifecycleOwner) {
+            when {
+                it.isError -> {
+                    Log.d("denemeee", it.errorMessage.toString())
+                }
+
+                else -> {
+                    Log.d("denemeee", it.books.joinToString())
+                }
+            }
+        }
+    }
+}
