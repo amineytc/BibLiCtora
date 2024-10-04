dec6ef5b19b7c74668f187bbc70d272e71fbaac2
amine <aytacamine633@gmail.com>
2024-10-04 14:44:26 +0300

    Add UseCase for ReadingStatus

---
+++ app/src/main/java/com/amineaytac/biblictora/core/domain/AddReadingBookItemUseCase.kt
@@ -0,0 +1,11 @@
+package com.amineaytac.biblictora.core.domain
+
+import com.amineaytac.biblictora.core.data.model.ReadingBook
+import com.amineaytac.biblictora.core.data.repo.BookRepository
+import javax.inject.Inject
+
+class AddReadingBookItemUseCase @Inject constructor(private val bookRepository: BookRepository) {
+    suspend operator fun invoke(readingBook: ReadingBook) {
+        return bookRepository.addReadingBookItem(readingBook)
+    }
+}
---
+++ app/src/main/java/com/amineaytac/biblictora/core/domain/GetReadingBookItemsUseCase.kt
@@ -0,0 +1,12 @@
+package com.amineaytac.biblictora.core.domain
+
+import com.amineaytac.biblictora.core.data.model.ReadingBook
+import com.amineaytac.biblictora.core.data.repo.BookRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetReadingBookItemsUseCase @Inject constructor(private val bookRepository: BookRepository) {
+    suspend operator fun invoke(): Flow<List<ReadingBook>> {
+        return bookRepository.getReadingBookItems()
+    }
+}
