4064eb7337b1378854b85b2ed5b6cdb18b7a01f4
amine <aytacamine633@gmail.com>
2024-10-04 14:40:41 +0300

    Edit BiblictoraDatabase

--- app/src/main/java/com/amineaytac/biblictora/core/database/BiblictoraDatabase.kt
+++ app/src/main/java/com/amineaytac/biblictora/core/database/BiblictoraDatabase.kt
@@ -3,9 +3,14 @@ package com.amineaytac.biblictora.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.amineaytac.biblictora.core.database.dao.FavoriteDao
+import com.amineaytac.biblictora.core.database.dao.ReadingStatusDao
+import com.amineaytac.biblictora.core.database.entity.FavoriteEntity
+import com.amineaytac.biblictora.core.database.entity.ReadingStatusEntity
 
-@Database(entities = [FavoriteEntity::class], version = 1)
+@Database(entities = [FavoriteEntity::class, ReadingStatusEntity::class], version = 5)
 @TypeConverters(TypeConverter::class)
 abstract class BiblictoraDatabase : RoomDatabase() {
     abstract fun favoriteDao(): FavoriteDao
+    abstract fun readingStatusDao(): ReadingStatusDao
 }
