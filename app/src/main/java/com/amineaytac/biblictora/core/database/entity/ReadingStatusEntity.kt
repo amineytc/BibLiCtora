29acfbb2b95db22352c39a2d7f6b8256acdeb594
amine <aytacamine633@gmail.com>
2024-10-04 14:36:47 +0300

    Add ReadingStatusEntity

---
+++ app/src/main/java/com/amineaytac/biblictora/core/database/entity/ReadingStatusEntity.kt
@@ -0,0 +1,29 @@
+package com.amineaytac.biblictora.core.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.amineaytac.biblictora.core.data.model.ReadFormats
+
+@Entity(tableName = "reading_status_table")
+data class ReadingStatusEntity(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id")
+    val id: Int,
+    @ColumnInfo(name = "authors")
+    val authors: String,
+    @ColumnInfo(name = "bookshelves")
+    val bookshelves: List<String>,
+    @ColumnInfo(name = "languages")
+    val languages: String,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "formats")
+    val formats: ReadFormats,
+    @ColumnInfo(name = "image")
+    val image: String,
+    @ColumnInfo(name = "readingStates")
+    val readingStates: String,
+    @ColumnInfo(name = "readingPercentage")
+    val readingPercentage: Int
+)
