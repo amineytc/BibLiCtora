9c6566425ed6dfac779c0b19823a3465edd01f1e
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-10-07 16:47:13 +0300

    Create ReadingFragment

---
+++ app/src/main/java/com/amineaytac/biblictora/ui/reading/ReadingFragment.kt
@@ -0,0 +1,6 @@
+package com.amineaytac.biblictora.ui.reading
+
+import androidx.fragment.app.Fragment
+import com.amineaytac.biblictora.R
+
+class ReadingFragment : Fragment(R.layout.fragment_reading)
