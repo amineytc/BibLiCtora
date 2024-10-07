fe21dfa2dcb1520977197ae14b3d652ae305455e
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-10-07 21:58:49 +0300

    Add readingProgress in ReadingStatusEntity

--- app/src/main/java/com/amineaytac/biblictora/core/database/entity/ReadingStatusEntity.kt
+++ app/src/main/java/com/amineaytac/biblictora/core/database/entity/ReadingStatusEntity.kt
@@ -7,23 +7,14 @@ import com.amineaytac.biblictora.core.data.model.ReadFormats
 
 @Entity(tableName = "reading_status_table")
 data class ReadingStatusEntity(
-    @PrimaryKey(autoGenerate = false)
-    @ColumnInfo(name = "id")
-    val id: Int,
-    @ColumnInfo(name = "authors")
-    val authors: String,
-    @ColumnInfo(name = "bookshelves")
-    val bookshelves: List<String>,
-    @ColumnInfo(name = "languages")
-    val languages: String,
-    @ColumnInfo(name = "title")
-    val title: String,
-    @ColumnInfo(name = "formats")
-    val formats: ReadFormats,
-    @ColumnInfo(name = "image")
-    val image: String,
-    @ColumnInfo(name = "readingStates")
-    val readingStates: String,
-    @ColumnInfo(name = "readingPercentage")
-    val readingPercentage: Int
+    @PrimaryKey(autoGenerate = false) @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "authors") val authors: String,
+    @ColumnInfo(name = "bookshelves") val bookshelves: List<String>,
+    @ColumnInfo(name = "languages") val languages: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "formats") val formats: ReadFormats,
+    @ColumnInfo(name = "image") val image: String,
+    @ColumnInfo(name = "readingStates") val readingStates: String,
+    @ColumnInfo(name = "readingPercentage") val readingPercentage: Int,
+    @ColumnInfo(name = "readingProgress") val readingProgress: Int
 )
