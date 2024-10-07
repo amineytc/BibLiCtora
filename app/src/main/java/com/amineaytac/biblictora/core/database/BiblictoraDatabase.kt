83c258e6005e471a9dd7fe4ea9f8ab77039d1466
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-10-07 22:01:55 +0300

    Increase BiblictoraDatabase version

--- app/src/main/java/com/amineaytac/biblictora/core/database/BiblictoraDatabase.kt
+++ app/src/main/java/com/amineaytac/biblictora/core/database/BiblictoraDatabase.kt
@@ -8,7 +8,7 @@ import com.amineaytac.biblictora.core.database.dao.ReadingStatusDao
 import com.amineaytac.biblictora.core.database.entity.FavoriteEntity
 import com.amineaytac.biblictora.core.database.entity.ReadingStatusEntity
 
-@Database(entities = [FavoriteEntity::class, ReadingStatusEntity::class], version = 5)
+@Database(entities = [FavoriteEntity::class, ReadingStatusEntity::class], version = 6)
 @TypeConverters(TypeConverter::class)
 abstract class BiblictoraDatabase : RoomDatabase() {
     abstract fun favoriteDao(): FavoriteDao
