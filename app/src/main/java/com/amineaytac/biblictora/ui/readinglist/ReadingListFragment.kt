bf918778314c29c60a1d1998c9a0235a441cc7aa
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-10-07 14:15:18 +0300

    Observe and bind view model data

--- app/src/main/java/com/amineaytac/biblictora/ui/readinglist/ReadingListFragment.kt
+++ app/src/main/java/com/amineaytac/biblictora/ui/readinglist/ReadingListFragment.kt
@@ -1,12 +1,14 @@
 package com.amineaytac.biblictora.ui.readinglist
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.amineaytac.biblictora.R
 import com.amineaytac.biblictora.databinding.FragmentReadingListBinding
+import com.amineaytac.biblictora.util.gone
+import com.amineaytac.biblictora.util.visible
 import com.amineaytc.biblictora.util.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,22 +17,45 @@ class ReadingListFragment : Fragment(R.layout.fragment_reading_list) {
 
     private val binding by viewBinding(FragmentReadingListBinding::bind)
     private val viewModel: ReadingListViewModel by viewModels()
+    private lateinit var bookAdapter: ReadingListAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         viewModel.getReadingBooks()
         observeUi()
+        bindBookAdapter()
+    }
+
+    private fun bindBookAdapter() = with(binding) {
+
+        bookAdapter = ReadingListAdapter {}
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = bookAdapter
     }
 
     private fun observeUi() = with(binding) {
         viewModel.readingBookScreenUiState.observe(viewLifecycleOwner) {
             when {
                 it.isError -> {
-                    Log.d("denemeee", it.errorMessage.toString())
+                    tvInfo.text = it.errorMessage
+                    tvInfo.visible()
+                    progressBar.gone()
                 }
 
                 else -> {
-                    Log.d("denemeee", it.books.joinToString())
+                    if (it.books.isEmpty()) {
+                        tvInfo.setText(R.string.reading_list_empty)
+                        tvInfo.visible()
+                        progressBar.gone()
+                    } else {
+                        tvInfo.gone()
+                        bookAdapter.submitList(it.books) {
+                            progressBar.gone()
+                        }
+                    }
                 }
             }
         }
